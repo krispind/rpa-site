4b409d286c82ad629fe1c8d1fba94faa9df8f914
Krispin Dolbear <kdolbear@mower.com>
2016-06-29 18:03:24 -0400

    tweak js for parallax

--- source/assets/js/script.js
+++ source/assets/js/script.js
@@ -1,13 +1,32 @@
 $(function(){
 
-  var parallaxBackground = $('.parallax');
-  var bgPosition = parallaxBackground.css('background-position');
+  // should be one function
+  var punkSection = $('.punk-section');
+  var bgPosition = punkSection.css('background-position');
   bgHorPosition = bgPosition.split(' ');
   updatePage = function() {
     window.requestAnimationFrame(function() {
-      var windowScroll = $(window).scrollTop();
-      var scrollAdjust = windowScroll / 2;
-      parallaxBackground.css({
+      var scrollTop     = $(window).scrollTop(),
+          elementOffset = $(punkSection).offset().top,
+          distance      = (elementOffset - scrollTop);
+      var scrollAdjust = 0 - distance / 5;
+      punkSection.css({
+        'background-position':bgHorPosition[0]+scrollAdjust+'px'
+      });
+    });
+  };
+  scrollIntervalID = setInterval(updatePage, 10);
+
+  var androidSection = $('.android-section');
+  var bgPosition = androidSection.css('background-position');
+  bgHorPosition = bgPosition.split(' ');
+  updatePage = function() {
+    window.requestAnimationFrame(function() {
+      var scrollTop     = $(window).scrollTop(),
+          elementOffset = $(androidSection).offset().top,
+          distance      = (elementOffset - scrollTop);
+      var scrollAdjust = 0 - distance / 5;
+      androidSection.css({
         'background-position':bgHorPosition[0]+scrollAdjust+'px'
       });
     });
