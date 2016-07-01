c4a3d09d7085cecff531837f4a3404ac1fccacc7
Jared <jkerschner@mower.com>
2016-06-30 20:12:29 -0400

    Added youtube code and google analytics

--- source/assets/js/script.js
+++ source/assets/js/script.js
@@ -33,4 +33,34 @@ $(function(){
   };
   scrollIntervalID = setInterval(updatePage, 10);
 
+
+  $('.yt-player-autoload').each(function(){
+      containerVideoID = $(this).attr('id');
+      setTimeout(function(){
+        loadYT(containerVideoID, 0);
+      },500);
+  });
+
 });
+
+function loadYT(id, autoplay) {
+  //id is the videoID for YouTube as well as the ID for the container we are targeting
+  embeddedPlayer = new YT.Player(id, {
+      playerVars: { 'autoplay': autoplay, 'controls': 2,'autohide':1,'wmode':'opaque','rel':0 },
+      videoId: id,
+      events: {
+          'onStateChange': embeddedOnPlayerStateChange
+      }
+  });
+}
+
+function embeddedOnPlayerStateChange(event) {
+  switch (event.data){
+    case YT.PlayerState.PLAYING:
+        ga('send', 'event', 'Video', 'Play', 'RPA Video');
+      break;
+    case YT.PlayerState.ENDED:
+        ga('send', 'event', 'Video', 'Complete', 'RPA Video');
+      break;
+  }
+}
